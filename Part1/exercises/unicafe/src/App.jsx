7a8264e9630956a151efccfcfc79c586fefb53ef
joshieyu <jyemails1@gmail.com>
2024-06-26 12:04:22 -0400

    Finished up to 1.11, starting 1.12

--- Part1/exercises/unicafe/src/App.jsx
+++ Part1/exercises/unicafe/src/App.jsx
@@ -6,10 +6,62 @@ const Button = (props) => (
     </button>
 )
 
-const Display = (props) => {
+const StatisticLine = (props) => {
   return (
     <div>
       {props.type}: {props.value}
+    </div>
+  )
+}
+
+
+const Statistics = (props) => {
+  if (props.total == 0)
+    {
+      return (
+        <div>
+          {"\nNo feedback given"}
+        </div>
+      )
+    }
+  else if (props.type == "lines") return (
+    <div>
+      <StatisticLine type="good" value={props.good} />
+      <StatisticLine type="neutral" value={props.neutral} />
+      <StatisticLine type="bad" value={props.bad} />
+      <StatisticLine type="total" value={props.total} />
+      <StatisticLine type="average" value={props.average} />
+      <StatisticLine type="positive" value={props.positive} />
+    </div>
+  )
+  else return (
+    <div>
+      <table>
+        <tr>
+          <td>good: </td>
+          <td> {props.good}</td>
+        </tr>
+        <tr>
+          <td>neutral: </td>
+          <td> {props.neutral}</td>
+        </tr>
+        <tr>
+          <td>bad: </td>
+          <td> {props.bad}</td>
+        </tr>
+        <tr>
+          <td>total: </td>
+          <td> {props.total}</td>
+        </tr>
+        <tr>
+          <td>average: </td>
+          <td> {props.average}</td>
+        </tr>
+        <tr>
+          <td>positive: </td>
+          <td> {props.positive}</td>
+        </tr>
+      </table>
     </div>
   )
 }
@@ -19,6 +71,9 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const total = good + neutral + bad
+  const average = (good - bad) / total
+  const positive = (good / total)
 
   return (
     <div>
@@ -31,12 +86,7 @@ const App = () => {
       <h1>
         statistics
       </h1>
-      <Display type="good" value={good} />
-      <Display type="neutral" value={neutral} />
-      <Display type="bad" value={bad} />
-
-
-
+      <Statistics type={"table"} good={good} bad={bad} neutral={neutral} total={total} average={average} positive={positive} />
     </div>
   )
 }
